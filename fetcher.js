a02c0159875f3498ec511b937f7626ec07cf6122
BelenMaru <bleneee@gmail.com>
2021-08-26 20:28:58 +0000

    add pagedowloader file

---
+++ fetcher.js
@@ -0,0 +1,29 @@
+
+
+const fs = require("fs");
+const request = require("request");
+
+
+const argv = process.argv.slice(2);
+const myURL = argv[0];
+const myPath = argv[1];
+
+request(myURL, (error, response, body) => {
+  if (error === null && response.statusCode === 200) {
+    fs.writeFile(myPath, body, err => {
+      if (err) {
+        console.error(err)
+        return
+      }
+      fs.stat(myPath, (err, fileStats) => {
+        if (err) {
+          console.log(err)
+        } else {
+          // console.log(fileStats)
+          console.log(`Downloaded and saved ${fileStats.size} bytes to ./index.html.`)
+        }
+      });
+    });
+  }
+
+});
